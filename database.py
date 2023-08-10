0e9e7e56c55e44a06f02de1f3987cbba951eb990
Yar1x <richtrik.j@gmail.com>
2023-08-10 18:24:03 +0200

    Přidání projektu

---
+++ database.py
@@ -0,0 +1,204 @@
+import asyncio
+import couchdb
+import requests
+
+# Loading data with ids using HTTP Request _bulk_get
+
+# Server setup
+
+server = couchdb.Server('http://admin:password@localhost:5984/')
+
+# Name of the database
+db_name = 'students'
+
+# Check if DB exists with the given name
+if db_name in server:
+    print("Database already exists.")
+else:
+    # Create a new database
+    db = server.create(db_name)
+    print("Database created:", db)
+
+db = server[db_name]
+
+
+# Creating a function that represents _bulk_get http request
+def bulk_get_data(db_url, docs_payload):
+    endpoint = f"{db_url}/_bulk_get"
+
+    try:
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+        response = requests.post(endpoint, json=docs_payload, headers=headers)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+# Creating a structure of documents
+student_documents = [
+    {
+        "_id": "student_id_1",
+        "name": "Alice",
+        "surname": "McKenzie",
+        "age": 20,
+        "address": {
+            "street": "769 Villa Drive",
+            "city": "South Bend",
+            "state": "IN",
+            "zip": "46618"
+        },
+    },
+    {
+        "_id": "student_id_2",
+        "name": "Bob",
+        "surname": "Cody",
+        "age": 25,
+        "address": {
+            "street": "1151 Kyle Street",
+            "city": "Grand Island",
+            "state": "NE",
+            "zip": "68801"
+        },
+    },
+    {
+        "_id": "student_id_3",
+        "name": "Charlie",
+        "surnme": "Shein",
+        "age": 18,
+        "address": {
+            "street": "2453 Heather Sees Way",
+            "city": "Welch",
+            "state": "OK",
+            "zip": "74369"
+        },
+    },
+    {
+        "_id": "student_id_4",
+        "name": "David",
+        "surname": "Bronwick",
+        "age": 22,
+        "address": {
+            "street": "2843 Bassell Avenue",
+            "city": "Wichita",
+            "state": "AR",
+            "zip": "67202"
+        },
+    },
+    {
+        "_id": "student_id_5",
+        "name": "Eve",
+        "surname": "Byrne",
+        "age": 20,
+        "address": {
+            "street": "1578 Catherine Drive",
+            "city": "Jamestown",
+            "state": "ND",
+            "zip": "58401"
+        },
+    }
+]
+
+teacher_documents = [
+    {
+        "_id": "teacher_id_1",
+        "name": "John",
+        "surname": "Doe",
+        "age": 35,
+        "address": {
+            "street": "123 Main Street",
+            "city": "Anytown",
+            "state": "Somestate",
+            "zipcode": "12345"
+            }
+    },
+    {
+        "_id": "teacher_id_2",
+        "name": "Jane",
+        "surname": "Smith",
+        "age": 42,
+        "address": {
+            "street": "456 Elm Street",
+            "city": "Othertown",
+            "state": "Otherstate",
+            "zipcode": "54321"
+            }
+    },
+    {
+        "_id": "teacher_id_3",
+        "name": "Robert",
+        "surname": "Johnson",
+        "age": 28,
+        "address": {
+            "street": "789 Oak Avenue",
+            "city": "Anycity",
+            "state": "Anotherstate",
+            "zipcode": "98765"
+            }
+    }
+
+]
+
+class_documents = [
+    {
+        "_id": "class_id_1",
+        "name": "EL",
+        "teacher": "teacher_id_1",
+        "students": ["student_1", "student_3"]
+    },
+    {
+        "_id": "class_id_2",
+        "name": "KB",
+        "teacher": "teacher_id_3",
+        "students": ["student_5"]
+    },
+    {
+        "_id": "class_id_3",
+        "name": "STR",
+        "teacher": "teacher_id_2",
+        "students": ["student_2", "student_4"]
+    }
+]
+
+
+
+
+all_documents = student_documents + teacher_documents + class_documents
+
+
+# Creating documents
+# for docs in all_documents:
+#     db.save(docs)
+
+# Test if documents were inserted
+print("Inserted documents:")
+for doc_id in db:
+    doc = db[doc_id]
+    print(f"''Doc id: {doc['_id']} ; Name: {doc['name']}")
+
+db_url = "http://admin:password@localhost:5984/students"
+document_ids = {
+    "docs": [
+
+    
+    {
+       "id": "student_id_1"
+    },
+    {"id": "student_id_5"},
+    {"id": "teacher_id_3"},
+    {"id":"class_id_2"}
+    
+    ]
+}
+print("\nBulk data\n")
+result = bulk_get_data(db_url, document_ids)
+if result:
+    print(f"{result} \n")
+else:
+    print("Failed to fetch data.")
+
+#curl -X POST http://admin:password@localhost:5984/students/_bulk_get -H "Accept: application/json" -H "Content-Type: application/json" -d "{\"docs\": [{\"id\": \"student_id_1\"}, {\"id\":\"student_id_5\"},{\"id\": \"teacher_id_3\"},{\"id\":\"class_id_2\"}]}"
---
+++ database3.py
@@ -0,0 +1,97 @@
+import aiohttp
+from aiohttp.client_exceptions import ClientResponseError
+import asyncio
+
+# Data loading using the aiohttp and asyncio libraries
+#   inspired by hrbolek/uoishelepres
+
+class CouchDBDataLoader:
+    def __init__(self, db_url, db_name):
+        """
+        The function initializes a class instance with a database URL, database name, batch size, and a
+        client session.
+        
+        :param db_url: The `db_url` parameter is the URL or address of the database you want to connect
+        to. It should be a string that specifies the location of the database server
+        :param db_name: The `db_name` parameter is the name of the database you want to connect to. It
+        is used to specify which database you want to interact with when making database queries or
+        operations
+        """
+        self.db_url = db_url
+        self.db_name = db_name
+        self.batch_size = 10  # Set your desired batch size here
+        self.session = aiohttp.ClientSession()
+
+    async def fetch_documents(self, keys):
+        """
+        The function fetches documents from a specified database using a POST request and returns the
+        values of the documents.
+        
+        :param keys: The `keys` parameter is a list of keys that you want to use for fetching documents
+        from the database. These keys will be used to filter the documents returned by the view
+        specified in the URL
+        :return: a list of values from the "value" field of each row in the response data.
+        """
+        try:
+            async with self.session.post(
+                f"{self.db_url}/{self.db_name}/_design/my_views/_view/id_view",
+                json={"keys": keys},
+                headers={"Content-Type": "application/json"},
+            ) as response:
+                response_data = await response.json()
+              #  print(response_data)
+                #documents = [{"_id": row["id"], "name": row["value"]} for row in response_data["rows"]]
+                #return documents
+                return [row["value"] for row in response_data["rows"]]
+        except ClientResponseError as e:
+            print(f"Error fetching documents: {e}")
+            return []
+
+    async def batch_load_fn(self, keys):
+        """
+        The function `batch_load_fn` asynchronously fetches documents based on a list of keys and
+        returns a list of documents in the same order as the input keys.
+        
+        :param keys: The `keys` parameter is a list of keys that need to be loaded. These keys are used
+        to fetch documents from a data source
+        :return: a list of documents corresponding to the input keys. If a document is not found for a
+        particular key, None is returned in its place.
+        """
+        datamap = {}
+        not_found_keys = set(keys)
+        while not_found_keys:
+            current_keys = list(not_found_keys)[: self.batch_size]
+            not_found_keys -= set(current_keys)
+
+            documents = await self.fetch_documents(current_keys)
+            for doc in documents:
+                datamap[doc["_id"]] = doc
+
+        result = [datamap.get(id, None) for id in keys]
+        return result
+
+    async def close(self):
+        """
+        The function closes the session.
+        """
+        await self.session.close()
+
+async def main():
+    """
+    The main function loads data from a CouchDB database using a batch load function and prints the
+    result.
+    """
+    db_url = "http://admin:password@localhost:5984"
+    db_name = "students"
+
+    loader = CouchDBDataLoader(db_url, db_name)
+    keys = ["class_id_2", "teacher_id_1", "student_id_3"]
+    result = await loader.batch_load_fn(keys)
+    print(result)
+
+    await loader.close()
+
+# The `if __name__ == "__main__"` block is a common Python idiom that allows a script to be executed
+# as a standalone program or imported as a module.
+if __name__ == "__main__":
+    asyncio.run(main())
